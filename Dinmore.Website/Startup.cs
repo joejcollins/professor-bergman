c586e3066c664fe934ea3778e31dbd04b8962389
RyanONeill1970 <r@ryanoneill.com>
2015-05-08 16:54:09 +0100

    Initial commit.

---
+++ Dinmore.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dinmore.Website.Startup))]
+namespace Dinmore.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
