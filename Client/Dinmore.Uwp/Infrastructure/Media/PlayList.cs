a3addbf0f5137a81debcfafbbd2b401523b93531
Martin Beeby <martinbeeby@hotmail.com>
2017-04-06 15:43:46 +0100

    Adding Sound Files, And Playlist logic

--- Client/Dinmore.Uwp/Infrastructure/Media/PlayList.cs
+++ Client/Dinmore.Uwp/Infrastructure/Media/PlayList.cs
@@ -11,8 +11,10 @@ namespace Dinmore.Uwp.Infrastructure.Media
                 return new List<PlayListItem>
             {
                 new PlayListItem( PlayListGroup.SingleFace, 1, "Assets/Voice/0-12-17-hiya.wav"),
-                new PlayListItem( PlayListGroup.SingleFace, 2, "Assets/Voice/goat.wav"),
-                new PlayListItem( PlayListGroup.SingleFace, 3, "Assets/Voice/sheep.wav"),
+                new PlayListItem( PlayListGroup.SingleFace, 2, "Assets/Voice/1-55-64.wav"),
+                new PlayListItem( PlayListGroup.SingleFace, 3,  "Assets/Voice/2-55-64.wav"),
+                new PlayListItem( PlayListGroup.SingleFace, 3,  "Assets/Voice/3-55-64.wav"),
+                new PlayListItem( PlayListGroup.SingleFace, 3,  "Assets/Voice/4-55-64.wav"),
                 new PlayListItem( PlayListGroup.MultiFace, 1, "Assets/Voice/0-12-17-helloeveryone.wav"),
             };
             }
