dc494820815f9ad10631e1f845176a1236aafab2
Martin Beeby <martinbeeby@hotmail.com>
2017-04-07 13:32:33 +0100

    Fiixed Playlist Order

--- Client/Dinmore.Uwp/Infrastructure/Media/PlayList.cs
+++ Client/Dinmore.Uwp/Infrastructure/Media/PlayList.cs
@@ -10,14 +10,12 @@ namespace Dinmore.Uwp.Infrastructure.Media
             {
                 return new List<PlayListItem>
             {
-                new PlayListItem( PlayListGroup.Demographic12to17, 2, "Assets/Voice/1-55-64.wav"),
-                new PlayListItem( PlayListGroup.Demographic12to17, 3, "Assets/Voice/2-55-64.wav"),
+                new PlayListItem( PlayListGroup.Demographic12to17, 1, "Assets/Voice/1-55-64.wav"),
+                new PlayListItem( PlayListGroup.Demographic12to17, 2, "Assets/Voice/2-55-64.wav"),
                 new PlayListItem( PlayListGroup.Demographic12to17, 3, "Assets/Voice/3-55-64.wav"),
-                new PlayListItem( PlayListGroup.Demographic12to17, 3, "Assets/Voice/4-55-64.wav"),
+                new PlayListItem( PlayListGroup.Demographic12to17, 4, "Assets/Voice/4-55-64.wav"),
                 new PlayListItem( PlayListGroup.HelloSingleFace, 1, "Assets/Voice/hello.wav"),
-                new PlayListItem( PlayListGroup.HelloSingleFace, 1, "Assets/Voice/hiya.wav"),
                 new PlayListItem( PlayListGroup.HelloMultipleFace, 1, "Assets/Voice/helloeveryone.wav"),
-                new PlayListItem( PlayListGroup.HelloMultipleFace, 1, "Assets/Voice/helloall.wav")
             };
             }
         }
