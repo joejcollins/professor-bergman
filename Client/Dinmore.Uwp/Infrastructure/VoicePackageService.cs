14c76daba0642fa1062efd79a7f84c0db05991e8
Martin Beeby <martinbeeby@hotmail.com>
2017-08-31 11:41:47 +0100

    Adding the ability to Unpack a voice

---
+++ Client/Dinmore.Uwp/Infrastructure/VoicePackageService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Dinmore.Uwp.Infrastructure
+{
+    public class VoicePackageService
+    {
+        public async static void UnpackVoice(string voiceGUID) {
+            // Will be null if folder dosen't exist
+            var storageFolder = await ApplicationData.Current.LocalFolder.TryGetItemAsync("Assets\\Voice\\");
+            if (storageFolder == null) {
+                storageFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync("Assets\\Voice\\");
+            }
+                        
+            var outputFolderLocation = $"Assets\\Voice\\{voiceGUID}\\";
+            var outputputFolder = await ApplicationData.Current.LocalFolder.TryGetItemAsync(outputFolderLocation);
+            if (outputputFolder != null)
+            {
+                await outputputFolder.DeleteAsync(StorageDeleteOption.PermanentDelete);
+            }
+            System.IO.Compression.ZipFile.ExtractToDirectory($"{storageFolder.Path}\\{voiceGUID}.zip", storageFolder.Path);
+        }
+    }
+}
