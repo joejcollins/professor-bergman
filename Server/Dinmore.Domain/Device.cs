fe5cb5683be5dcbe80fc9d826130bb1198f8c34a
Joe Colllins <joejcollins@gmail.com>
2017-09-20 14:56:07 +0100

    A few descriptions, as yet unused.

--- Server/Dinmore.Domain/Device.cs
+++ Server/Dinmore.Domain/Device.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿/*
+ * 
+ */
+using System;
+using System.ComponentModel.DataAnnotations;
 
+/// <summary>
+/// 
+/// </summary>
 namespace Dinmore.Domain
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Device
     {
         public Guid Id { get; set; }
+
 
         public string DeviceLabel { get; set; }
 
@@ -12,12 +23,16 @@ namespace Dinmore.Domain
 
         public string Venue { get; set; }
 
+        [Display(Name = "Is Listening?", 
+            Description = "Is listening to the visitor speaking?")]
         public bool Interactive { get; set; }
 
         public bool VerbaliseSystemInformationOnBoot { get; set; }
 
         public bool SoundOn { get; set; }
 
+        [Display(Name = "Reset on start up?", 
+            Description = "Will the device get new settings from the server on start up?")]
         public bool ResetOnBoot { get; set; }
 
         public string VoicePackageUrl { get; set; }
