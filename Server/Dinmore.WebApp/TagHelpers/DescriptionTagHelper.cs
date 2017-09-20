2fa2c8f3fb81176bcfe505d3c93bbb7b019fd73e
joejcollins <joejcollins@hotmail.com>
2017-09-20 23:22:16 +0100

    Added description helper

---
+++ Server/Dinmore.WebApp/TagHelpers/DescriptionTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dinmore.WebApp.TagHelpers
+{
+    [HtmlTargetElement("description")]
+    public class DescriptionTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";    
+        }
+    }
+}
