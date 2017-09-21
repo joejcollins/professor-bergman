1f5bb32aaf1375c0dc52dfd50e6477d20fd77785
Joe Colllins <joejcollins@gmail.com>
2017-09-21 11:12:36 +0100

    Tag helper giving metadata descriptions as HTML titles

--- Server/Dinmore.Domain/Device.cs
+++ Server/Dinmore.Domain/Device.cs
@@ -14,27 +14,40 @@ namespace Dinmore.Domain
     /// </summary>
     public class Device
     {
+        [Display(Name = "ID",
+            Description = "Unique identifier for the device, this can be changed by presenting a new QR code")]
         public Guid Id { get; set; }
 
-
+        [Display(Name = "Device Label",
+           Description = "Where on the exhibit is device mounted?")]
         public string DeviceLabel { get; set; }
 
+        [Display(Name = "Exhibit",
+            Description = "Which exhibit is the device mounted on?")]
         public string Exhibit { get; set; }
 
+        [Display(Name = "Venue",
+            Description = "The name of the museum, gallery or location of the exhibit")]
         public string Venue { get; set; }
 
         [Display(Name = "Is Listening?", 
             Description = "Is listening to the visitor speaking?")]
         public bool Interactive { get; set; }
 
+        [Display(Name = "Speak System Info on Start?",
+            Description = "Verbalise the system information on start up?")]
         public bool VerbaliseSystemInformationOnBoot { get; set; }
 
+        [Display(Name = "Is Sound On?",
+            Description = "Is the sound on so the exhibit can speak to the visitor?")]
         public bool SoundOn { get; set; }
 
-        [Display(Name = "Reset on start up?", 
+        [Display(Name = "Reset on Start?", 
             Description = "Will the device get new settings from the server on start up?")]
         public bool ResetOnBoot { get; set; }
 
+        [Display(Name = "Voice Package Url",
+            Description = "Web address of the zip file containing the voices")]
         public string VoicePackageUrl { get; set; }
 
         public string QnAKnowledgeBaseId { get; set; }
